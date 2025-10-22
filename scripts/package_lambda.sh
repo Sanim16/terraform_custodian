d4d483b2724ef1ec96b4a8fe31ef61618e32ebf5
Momoh Sani Musa <sanim16@yahoo.com>
2025-10-22 22:39:05 +0100

    update commit

--- scripts/package_lambda.sh
+++ scripts/package_lambda.sh
@@ -1,3 +1,24 @@
+# #!/bin/bash
+# set -e
+
+# BUILD_DIR="build"
+# LAMBDA_DIR="lambda"
+# ZIP_FILE="$BUILD_DIR/custodian_lambda.zip"
+
+# mkdir -p "$BUILD_DIR"
+
+# # Use Docker to build in Amazon Linux compatible env
+# docker run --rm -v "$PWD/$LAMBDA_DIR":/var/task -w /var/task \
+#   public.ecr.aws/lambda/python:3.9 \
+#   /bin/bash -c "
+#     pip install -r requirements.txt -t ./package && \
+#     cp handler.py custodian-policy.yml ./package && \
+#     cd package && \
+#     zip -r9 ../custodian_lambda.zip ."
+
+# mv "$LAMBDA_DIR/custodian_lambda.zip" "$BUILD_DIR/"
+# echo "✅ Lambda package built at $ZIP_FILE"
+
 #!/bin/bash
 set -e
 
@@ -7,10 +28,11 @@ ZIP_FILE="$BUILD_DIR/custodian_lambda.zip"
 
 mkdir -p "$BUILD_DIR"
 
-# Use Docker to build in Amazon Linux compatible env
+# Use Docker to build in Amazon Linux compatible env, bypassing entrypoint
 docker run --rm -v "$PWD/$LAMBDA_DIR":/var/task -w /var/task \
+  --entrypoint /bin/bash \
   public.ecr.aws/lambda/python:3.9 \
-  /bin/bash -c "
+  -c "
     pip install -r requirements.txt -t ./package && \
     cp handler.py custodian-policy.yml ./package && \
     cd package && \
