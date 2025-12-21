c64c98edb6167e7797338cb964cc53839ca2b8fa
Momoh Sani Musa <sanim16@yahoo.com>
2025-12-21 11:41:14 +0100

    custodian-update

--- README.md
+++ README.md
@@ -1,14 +1,3 @@
-
-# Terraform Cloud Custodian deployment
-
-[![Github Actions](https://github.com/Sanim16/terraform_custodian/actions/workflows/deploy.yml/badge.svg)](https://github.com/Sanim16/terraform_custodian/actions/workflows/deploy.yml)
-
->This is a Terraform project that deploys a `cloudcustodian` using lambda.
-
-## Resources:
-- Terraform
-
-
 # Terraform Cloud Custodian on AWS (Off-Hours Automation)
 
 [![GitHub Actions](https://github.com/Sanim16/terraform_custodian/actions/workflows/deploy.yml/badge.svg)](https://github.com/Sanim16/terraform_custodian/actions/workflows/deploy.yml)
--- lambda.tf
+++ lambda.tf
@@ -2,13 +2,13 @@
 # Creating lambda layer for requests python library
 ################################################################################
 resource "aws_lambda_layer_version" "custodian_layer" {
-  s3_bucket = aws_s3_bucket.custodian_bucket.id
-  s3_key    = aws_s3_object.my_file_upload.key
+  s3_bucket           = aws_s3_bucket.custodian_bucket.id
+  s3_key              = aws_s3_object.my_file_upload.key
   layer_name          = local.layer_name
   compatible_runtimes = ["${var.runtime}"]
   depends_on          = [aws_s3_object.my_file_upload]
   source_code_hash    = data.aws_s3_object.layer_object.checksum_sha256
-  description = "Cloud Custodian (c7n) Lambda Layer"
+  description         = "Cloud Custodian (c7n) Lambda Layer"
 }
 
 data "aws_s3_object" "layer_object" {
@@ -31,11 +31,11 @@ data "archive_file" "lambda_function_archive" {
 resource "aws_lambda_function" "custodian_lambda_function" {
   function_name = var.function_name
   filename      = "${path.module}/lambda_function.zip"
-  runtime     = var.runtime
-  handler     = "custodian.lambda_handler"
-  layers      = [aws_lambda_layer_version.custodian_layer.arn]
-  memory_size = 512
-  timeout     = 300
+  runtime       = var.runtime
+  handler       = "custodian.lambda_handler"
+  layers        = [aws_lambda_layer_version.custodian_layer.arn]
+  memory_size   = 512
+  timeout       = 300
 
   role = aws_iam_role.lambda_role.arn
 
